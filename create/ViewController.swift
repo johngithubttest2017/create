af64b18450eefd1171b120b75cf47a325fe71ede
Administrator <admin@wma1.apple.com>
2018-02-01 09:56:39 -0800

    Initial Commit

---
+++ create/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  create
+//
+//  Created by Administrator on 2/1/18.
+//  Copyright © 2018 hehe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
